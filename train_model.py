389611529f14d68f329304928160e65387f7515a
ThiagoJFreitas <thiagojsf90@gmail.com>
2024-07-25 20:24:19 -0300

    Updates.

--- train_model.py
+++ train_model.py
@@ -17,7 +17,7 @@ trainGen = trainValGenerator(2, 'dataset/train', 'image', 'label', data_gen_args
 valGen = trainValGenerator(2, 'dataset/val', 'image', 'label', data_gen_args, save_image_to_dir=None, save_label_to_dir=None)
 
 model = unet()
-callbacks = get_callbacks('../Cell_Segmentation2.0/model/unet_model.h5')
-history = model.fit(trainGen, validation_data=valGen, epochs=50, steps_per_epoch=100, validation_steps=100, callbacks=callbacks)
+callbacks = get_callbacks('model/unet_model.h5')
+history = model.fit(trainGen, validation_data=valGen, epochs=10, steps_per_epoch=300, validation_steps=300, callbacks=callbacks)
 
 plot_training_history(history)
